2ce947ec88e3f22ee9b80b61b2da9fb7fa87fab5
lgramajo99 <lucianongramajo@gmail.com>
2023-02-21 00:42:03 -0300

    Arreglos de bienvenida listo

--- client/src/components/bienvenida/Bienvenida.jsx
+++ client/src/components/bienvenida/Bienvenida.jsx
@@ -2,19 +2,22 @@ import style from './Bienvenida.module.css'
 import { Link } from 'react-router-dom'
 
 function Bienvenida() {
-    let _pokeImagenBienvenida = "https://i.imgur.com/h6swayq.png"
+    let _pokeImagenBienvenida = "https://i.imgur.com/CP0DCbQ.png"
+    // const _contorno = "https://i.imgur.com/6bwXNT2.png";
+
     return (
         // <div className={style.divPadreBienvenida}>
+        <div className={style.divPadre}>
+            <div className={style.divBienvenida}>
+                <h2 className={style.titBienvenida}>Bievenido a PokeApi</h2>
+                <p className={style.pBienvenida}>Entra y encuentra todos los pokemones que existen, busca a tus favoritos y crea tu propio equipo y empieza la aventura.</p>
 
-        <div className={style.divBienvenida}>
-            <h1 className={style.titBienvenida}>Bievenido a PokeApi</h1>
-            <p className={style.pBienvenida}>Entra y encuentra todos los pokemones que existen, busca a tus favoritos y crea tu propio equipo y empieza la aventura.</p>
+                <Link to={'/inicio'} className={style.btnBienvenida}>Ingresar como invitado</Link>
+                <Link to={'/inicio'} className={style.btnBienvenida}>Ingresar como administrador</Link>
 
-            <Link to={'/inicio'}><button className={style.btnBienvenida}>Ingresar</button></Link>
-
+            </div>
             <img className={style.imgSnorlaxBienvenida} src={_pokeImagenBienvenida} alt="Snorlax saludando" />
-        </div >
-        // </div>
+        </div>
     )
 }
 
